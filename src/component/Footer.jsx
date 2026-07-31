8aff0cc1ba742cfadc7c51a1906736d0ac636a67
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-31 08:46:15 +0600

    added

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div>
             <h1 className="text-center text-sm text-muted-foreground font-semibold border-t-2">
-                © 2023 Dev-Story. All rights reserved.
+                © 202 Dev-Story. All rights reserved-ABDULLAH ALL MOJAHID
             </h1>
         </div>
     );
