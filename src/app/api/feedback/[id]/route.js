1453c2692814e07d162a1cd92e42830d94b23615
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-08-08 13:49:34 +0600

    added

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -1,18 +1,17 @@
 import { connect } from "@/app/lib/dbConnect";
 import { feedback } from "../../route";
 
-export async function GET(request , {params}) {
-    const { id } = params;
+export async function GET(request, { params }) {
+  const { id } = params;
 
-    const singleFeedback = feedback.find((item) => item.id === parseInt(id));
+  const singleFeedback = feedback.find((item) => item.id === parseInt(id));
 
-    const feedbackCollection = connect("feedback");
-    const feedbackFromDB = await feedbackCollection.find().toArray();
+  const feedbackCollection = connect("feedback");
+  const feedbackFromDB = await feedbackCollection.find().toArray();
 
-    
-    return Response.json({
-        status: 200,
-        message: "Feedback retrieved successfully",
-        data: singleFeedback || feedbackFromDB,
-    });     
+  return Response.json({
+    status: 200,
+    message: "Feedback retrieved successfully",
+    data: singleFeedback || feedbackFromDB,
+  });
 }
