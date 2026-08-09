0c5d225775cc43b0a8c3d2c955c29861a391e091
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-08-09 19:28:48 +0600

    added

--- src/app/lib/dbConnect.js
+++ src/app/lib/dbConnect.js
@@ -1,21 +1,32 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.URI;
-const client = new MongoClient(uri);
 
-export async function connectToMongoDB() {
-  try {
-    await client.connect();
-    console.log("You successfully connected to MongoDB!");
-    return client;
-  } catch (err) {
-    console.dir(err);
+if (!uri) {
+  throw new Error("Please define URI in your .env file");
+}
+
+const options = {};
+
+let client;
+let clientPromise;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
   }
+
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
 }
 
-export const connect = (collection) => {
-    const db = client.db(process.env.DB_NAME);
-    return db.collection(collection); 
+export async function connect(collectionName) {
+  const client = await clientPromise;
+
+  const db = client.db(process.env.DB_NAME);
+
+  return db.collection(collectionName);
 }
-
-
